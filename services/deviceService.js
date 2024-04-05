c6e843f2e1046935019df7d15d46843702bcb11b
Alex <nta_aldan@hotmail.com>
2024-04-05 11:21:00 -0600

    changes to the way the distance gets sent

--- services/deviceService.js
+++ services/deviceService.js
@@ -66,8 +66,8 @@ function updateDistance(msg){
     })
     .then(result => {
     // Handle the result returned by the stored procedure
-      console.log("AAAA" + result[0]);
-      console.log("AAAA" + result[1]);
+      console.log("AAAA" + JSON.stringify(result[0]));
+      console.log("AAAA" + JSON.stringify(result[1]));
 
       return result;
     })
